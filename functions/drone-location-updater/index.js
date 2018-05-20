cd2fe75786a2278283ecdc6cae8aa386812f09ba
JB Claramonte <jbclaramonte@xebia.fr>
2018-05-20 12:05:35 +0200

    start working on push to topic drone-events

--- functions/drone-location-updater/index.js
+++ functions/drone-location-updater/index.js
@@ -1,9 +1,11 @@
 const Datastore = require('@google-cloud/datastore');
+const PubSub = require(`@google-cloud/pubsub`);
 const turf = require('@turf/turf');
 
-// creates datastore client
 const datastore = new Datastore({});
+const pubsub = new PubSub();
 
+const topicName = 'drone-events';
 const DISTANCE_PER_TICK = 0.1;
 
 exports.droneLocationUpdater = (req, res) => {
@@ -24,21 +26,48 @@ exports.droneLocationUpdater = (req, res) => {
             var currentLocation = turf.point([droneInfo.location.latitude, droneInfo.location.longitude]);
             var dest = turf.point([droneInfo.command.location.latitude, droneInfo.command.location.longitude]);
 
-            // todo : compute distance to dest and if less than the distance done by the drone each update the set
-            // current location with the destination distance and remove the command attribut
             const distance = turf.distance(currentLocation, dest, {});
             if (distance < DISTANCE_PER_TICK) {
                 console.log('drone has arrived at destination');
                 droneInfo.location.latitude = droneInfo.command.location.latitude;
                 droneInfo.location.longitude = droneInfo.command.location.longitude;
                 delete droneInfo.command;
+                // todo : envoyer un event DESTINATION_REACHED dans le topic drone-events
+                // todo : checker s'il y a un colis à prendre si c'est le cas le prendre et envoyer l'event PARCEL_GRABBED (et ne pas envoyer l'event DESTINATION_REACHED)
+
+                const data = JSON.stringify({ event: 'DESTINATION_REACHED' });
+                const dataBuffer = Buffer.from(data);
+                pubsub
+                    .topic(topicName)
+                    .publisher()
+                    .publish(dataBuffer)
+                    .then(messageId => {
+                        console.log(`Message ${messageId} published.`);
+                    })
+                    .catch(err => {
+                        console.error('ERROR:', err);
+                    });
             } else {
+                // Continue moving to destination
                 bearing = turf.bearing(currentLocation, dest);
                 console.log(`bearing for team ${JSON.stringify(droneInfo[datastore.KEY])}: ${bearing}`);
                 var destination = turf.destination(currentLocation, DISTANCE_PER_TICK, bearing, {});
                 console.log(`next point is: ${JSON.stringify(destination)}`);
                 droneInfo.location.latitude = destination.geometry.coordinates[0];
                 droneInfo.location.longitude = destination.geometry.coordinates[1];
+
+                const data = JSON.stringify({ event: 'MOVING' });
+                const dataBuffer = Buffer.from(data);
+                pubsub
+                    .topic(topicName)
+                    .publisher()
+                    .publish(dataBuffer)
+                    .then(messageId => {
+                        console.log(`Message ${messageId} published.`);
+                    })
+                    .catch(err => {
+                        console.error('ERROR:', err);
+                    });
             }
             console.log(`-- droneInfo after update : ${JSON.stringify(droneInfo)}`);
             upsert(droneInfo);
