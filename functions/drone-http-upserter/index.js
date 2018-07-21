f9d22aab8bec5e6eaca3cfd110c2f70bbda2a603
JB Claramonte <jbclaramonte@xebia.fr>
2018-07-21 12:30:19 +0200

    Fix correct way of ending http functions

--- functions/drone-http-upserter/index.js
+++ functions/drone-http-upserter/index.js
@@ -33,12 +33,11 @@ exports.droneHttpUpserter = (req, res) => {
                 .upsert(droneInfoEntity)
                 .then(() => {
                     console.log(`DroneInfo entity with id ${jsonMsg.teamId} upserted successfully.`);
-                    // Don't forget to call the callback.
                     res.status(200).end();
                 })
                 .catch(err => {
                     console.error('ERROR:', err);
-                    callback(new Error(err));
+                    res.status(500).end();
                 });
         }
 
