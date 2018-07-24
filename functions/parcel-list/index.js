26bcf629200fd10eb3ac6c6571dd42be14e796ef
JB Claramonte <jbclaramonte@xebia.fr>
2018-07-24 14:19:54 +0200

    Update parcel list to return all parcel related to a teamId

--- functions/parcel-list/index.js
+++ functions/parcel-list/index.js
@@ -1,18 +1,28 @@
 const Datastore = require('@google-cloud/datastore');
 
-
 const datastore = new Datastore({});
 
-
-/**
- * 
- * gcloud command to deploy:
- * gcloud beta functions deploy parcelList --trigger-http
- */
 exports.parcelList = (req, res) => {
     console.log('parcelList');
     console.log(`teamId = ${req.query.teamId}`);
 
-    res.send('should return a list of available Parcels for a given team');
+    const teamId = req.query.teamId;
+    const query = datastore
+        .createQuery('Parcel')
+        .filter('teamId', teamId);
+
+    datastore.runQuery(query)
+        .then(results => {
+            const parcels = results[0];
+
+            console.log('parcels:');
+            parcels.forEach(parcel => console.log(parcel));
+            const parcelsWithParcelId = parcels.map(p => p.parcelId = p[datastore.KEY].name);
+            res.status(200).send(parcelsWithParcelId);
+        })
+        .catch(err => {
+            console.error(`Oups cannot get data for teamId ${teamId}`, err);
+            res.status(500).send(err);
+        });
 };
 
