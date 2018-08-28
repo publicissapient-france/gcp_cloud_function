2e497b0664c49f74517aa2ee2f80bfe9ca3984f0
ModuloM <modulom@modulom.net>
2018-08-28 09:45:50 +0200

    Refactor solution drone-event-consumer function, juste log message from topic

--- solutions/drone-event-consumer/index.js
+++ solutions/drone-event-consumer/index.js
@@ -1,37 +1,8 @@
-const PubSub = require('@google-cloud/pubsub');
+exports.droneEventDispatcher = (data, context, callback) => {
+    const message = Buffer.from(data, 'base64').toString();
 
-exports.droneEventConsumer = (req, res) => {
-  // init pubsub
-  const pubsubClient = new PubSub();
+    console.log("event received " + message);
 
-  const topicName = 'projects/jbc-atl-sal-func-techevent/topics/drone-command';
-  const topic = pubsubClient.topic(topicName);
-  const publisher = topic.publisher();
-
-  // message
-  const teamId = 'blue';
-  const name = 'READY';
-  // const command = 'MOVE';
-  const message = {
-    teamId,
-    command: {
-      name,
-      // location : {
-      //   latitude : 45.3534,
-      //   longitude : 2.3535
-      // }
-    }
-  };
-  const dataToPublish = Buffer.from(JSON.stringify(message));
-
-  publisher
-    .publish(dataToPublish)
-    .then(messageId => {
-      console.log(`Message ${messageId} has been published in topic`);
-      res.send(message);
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-      res.status(500).send(new Error('Error when trying to publish event in Drone Command topic'));
-    });
+    callback();
 };
+
