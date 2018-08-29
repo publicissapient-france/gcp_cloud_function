8ec5c2fb77ef888ef55a0061f3ffa9d162aaea00
ModuloM <modulom@modulom.net>
2018-08-29 09:21:43 +0200

    Add topic url to drone-command-publisher

--- solutions/drone-command-publisher/index.js
+++ solutions/drone-command-publisher/index.js
@@ -1,40 +1,43 @@
 const PubSub = require('@google-cloud/pubsub');
 
 exports.droneCommandPublisher = (req, res) => {
-  // init pubsub
-  const pubsubClient = new PubSub();
+    // init pubsub
+    const pubsubClient = new PubSub();
 
-  const topicName = 'projects/jbc-atl-sal-func-techevent/topics/drone-command';
-  const topic = pubsubClient.topic(topicName);
-  const publisher = topic.publisher();
+    const topicName = 'projects/jbc-atl-sal-func-techevent/topics/drone-command';
+    const topic = pubsubClient.topic(topicName);
+    const publisher = topic.publisher();
 
-  // message
-  const teamId = 'blue';
-  // const name = 'READY';
-  const name = 'MOVE';
-  const message = {
-    teamId,
-    command: {
-      name,
-      location : {
-      //   latitude : 45.3534,
-      //   longitude : 2.3535
-      // Xebia
-        latitude: 48.8753487,
-        longitude: 2.3088396,
-      },
-    },
-  };
-  const dataToPublish = Buffer.from(JSON.stringify(message));
+    // message
+    const teamId = 'blue';
+    // const name = 'READY';
+    const name = 'MOVE';
+    const message = {
+        teamId,
+        command: {
+            name,
+            location: {
+                //   latitude : 45.3534,
+                //   longitude : 2.3535
+                // Xebia
+                latitude: 48.8753487,
+                longitude: 2.3088396,
+            },
+            topic: {
+                url: 'projects/modulom-moludom/topics/drone-events'
+            }
+        },
+    };
+    const dataToPublish = Buffer.from(JSON.stringify(message));
 
-  publisher
-    .publish(dataToPublish)
-    .then(messageId => {
-      console.log(`Message ${messageId} has been published in topic`);
-      res.send(message);
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-      res.status(500).send(new Error('Error when trying to publish event in Drone Command topic'));
-    });
+    publisher
+        .publish(dataToPublish)
+        .then(messageId => {
+            console.log(`Message ${messageId} has been published in topic`);
+            res.send(message);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+            res.status(500).send(new Error('Error when trying to publish event in Drone Command topic'));
+        });
 };
