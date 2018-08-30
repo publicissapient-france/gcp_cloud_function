5c0fb8a5b4c953f1de61620219c13b7b2f83221e
ModuloM <modulom@modulom.net>
2018-08-30 15:15:16 +0200

    Improve solutions/drone-event-consumer

--- solutions/drone-event-consumer/index.js
+++ solutions/drone-event-consumer/index.js
@@ -1,6 +1,8 @@
-exports.droneEventConsumer = (data, context, callback) => {
+const atob = require('atob');
 
-    console.log('event received with data: ', message);
+exports.droneEventConsumer = (message, context, callback) => {
+    const data = atob(message.data);
+    console.log('event received with data: ', data);
 
     callback();
 };
