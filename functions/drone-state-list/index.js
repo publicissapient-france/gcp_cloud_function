44236a25c1a97372c8e9fce560ed5a0596a3c400
ModuloM <modulom@modulom.net>
2018-09-05 23:41:56 +0200

    [drone-state-list]: Refactor drones model and get parcels

--- functions/drone-state-list/index.js
+++ functions/drone-state-list/index.js
@@ -2,36 +2,41 @@ const Datastore = require('@google-cloud/datastore');
 
 const datastore = new Datastore({});
 
-exports.droneStateList = (req, res) => {
+exports.droneStateList = async (req, res) => {
     console.log('read from datastore');
-
-    const query = datastore
-        .createQuery('DroneInfo');
 
     // Enable CORS
     res.set('Access-Control-Allow-Origin', "*");
     res.set('Access-Control-Allow-Methods', 'GET, POST');
 
-    datastore
-        .runQuery(query)
-        .then(results => {
-            let data = {};
-            // console.log(`DroneInfos from datastore: ${JSON.stringify(results, null, 2)}`);
-            const droneInfos = results[0];
-            droneInfos.map((droneInfo) => {
-                console.log(`Drone info key ${JSON.stringify(droneInfo[datastore.KEY], null, 2)}`)
-                const teamName = droneInfo[datastore.KEY].name;
-                data[teamName] = {
-                    team: teamName,
-                    data: droneInfo,
-                };
-            });
-            console.log(`Parsed data from DroneInfo: ${JSON.stringify(data, null, 2)}`);
+    try {
+        let data = {};
 
-            res.send(data);
-        })
-        .catch(err => {
-            console.error('ERROR:', err);
-            res.status(500).send(new Error('Error when trying to get data from DroneInfo'));
+        // get DroneInfo
+        const droneInfoQuery = datastore.createQuery('DroneInfo');
+        const droneInfoResults = await datastore.runQuery(droneInfoQuery);
+        // console.log(`DroneInfos from datastore: ${JSON.stringify(results, null, 2)}`);
+        data.drones = droneInfoResults[0];
+        data.drones = data.drones.map((droneInfo) => {
+            console.log(`Drone info key ${JSON.stringify(droneInfo[datastore.KEY], null, 2)}`);
+            const teamName = droneInfo[datastore.KEY].name;
+            return {
+                teamId: teamName,
+                data: droneInfo,
+            };
         });
-}
+
+        // get Parcel
+        const parcelQuery = datastore.createQuery('Parcel');
+        const parcelResults = await datastore.runQuery(parcelQuery);
+        data.parcels = parcelResults[0];
+
+        console.log(`Parsed data from DroneInfo and Parcel: ${JSON.stringify(data, null, 2)}`);
+
+        res.send(data);
+        
+    } catch (err) {
+        console.error('ERROR:', err);
+        res.status(500).send(new Error('Error when trying to get data from DroneInfo'));
+    }
+};
