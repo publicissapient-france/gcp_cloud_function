cbb251c4fb45544126a2a129b7b0242c0c3ff9ee
ModuloM <modulom@modulom.net>
2018-09-07 17:56:17 +0200

    [drone-state-list]: Add defensive code if data return nothing

--- functions/drone-state-list/index.js
+++ functions/drone-state-list/index.js
@@ -16,20 +16,23 @@ exports.droneStateList = async (req, res) => {
         const droneInfoQuery = datastore.createQuery('DroneInfo');
         const droneInfoResults = await datastore.runQuery(droneInfoQuery);
         // console.log(`DroneInfos from datastore: ${JSON.stringify(results, null, 2)}`);
-        data.drones = droneInfoResults[0];
+        data.drones = droneInfoResults[0] || [];
         data.drones = data.drones.map((droneInfo) => {
-            console.log(`Drone info key ${JSON.stringify(droneInfo[datastore.KEY], null, 2)}`);
-            const teamName = droneInfo[datastore.KEY].name;
-            return {
-                teamId: teamName,
-                data: droneInfo,
-            };
+            if (droneInfo) {
+                console.log(`Drone info key ${JSON.stringify(droneInfo[datastore.KEY], null, 2)}`);
+                const teamName = droneInfo[datastore.KEY].name;
+                return {
+                    teamId: teamName,
+                    data: droneInfo,
+                };
+            }
+            return {};
         });
 
         // get Parcel
         const parcelQuery = datastore.createQuery('Parcel');
         const parcelResults = await datastore.runQuery(parcelQuery);
-        data.parcels = parcelResults[0];
+        data.parcels = parcelResults[0] || [];
 
         console.log(`Parsed data from DroneInfo and Parcel: ${JSON.stringify(data, null, 2)}`);
 
