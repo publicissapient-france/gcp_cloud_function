3a62b8904e99fc46d6333937357ac46acf820cc2
ModuloM <modulom@modulom.net>
2018-09-10 03:43:51 +0200

    [parcel-http-updater]: Return error if teamId is not set

--- functions/parcel-http-upserter/index.js
+++ functions/parcel-http-upserter/index.js
@@ -25,6 +25,9 @@ exports.parcelHttpUpserter = async (req, res) => {
             const parcelFromBody = req.body;
             console.log(JSON.stringify(req.body));
 
+            if (req.body.teamId === undefined) {
+                res.status(400).send('No body with a teamId to upsert defined!');
+            }
             if (parcelFromBody.parcelId === undefined) {
                 parcelFromBody.parcelId = uuidv4();
             }
